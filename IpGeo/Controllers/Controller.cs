85998887f35aacdd4b124acf0f2c3da899a59f12
958290716 <958290716@qq.com>
2024-11-29 15:46:29 +0800

    easy start

---
+++ IpGeo/Controllers/Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using IpGeo;
+using IpGeo.api.Models;
+
+namespace IpGeo.Controllers
+{
+    public class Controller 
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IpGeoData> Create([FromBody] int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IpGeoData> Delete([FromBody] int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IpGeoData> Update([FromBody] int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IpGeo/api/Models/IpGeoData.cs
@@ -0,0 +1,12 @@
+﻿namespace IpGeo.api.Models
+{
+    public class IpGeoData
+    {
+        public int ipStart {  get; set; }
+        public int ipEnd { get; set; }
+
+        public string regionName {  get; set; } = string.Empty;
+        public string countryName {  get; set; } = string.Empty;
+        public string cityName { get; set; } = string.Empty;
+    }
+}
