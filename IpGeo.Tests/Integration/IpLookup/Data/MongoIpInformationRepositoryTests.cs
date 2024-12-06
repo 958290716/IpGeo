437f61ee3c0bec51625a4fab65d96af83fe7b228
958290716 <958290716@qq.com>
2024-12-06 15:38:50 +0800

    Test change

--- IpGeo.Tests/Integration/IpLookup/Data/MongoIpInformationRepositoryTests.cs
+++ IpGeo.Tests/Integration/IpLookup/Data/MongoIpInformationRepositoryTests.cs
@@ -7,96 +7,89 @@ using Docker.DotNet.Models;
 
 namespace IpGeo.Tests.Integration.IpLookup.Data
 {
-    public class MongoIpInformationRepositoryTests
+    public class MongoDbContainerFixture : IAsyncLifetime
     {
-        /// <summary>
-        /// System under test
-        /// </summary>
-        public class MongoDbContainerFixture : IAsyncLifetime
+        private readonly MongoDbContainer _mongoDbContainer;
+        public string ConnectionString = ""; // => _mongoDbContainer.GetConnectionString();      
+
+        public MongoDbContainerFixture()
         {
-            private readonly MongoDbContainer _mongoDbContainer;
-            public string ConnectionString = ""; // => _mongoDbContainer.GetConnectionString();      
-
-            public MongoDbContainerFixture()
-            {
-                // 创建 MongoDB 容器配置
-                _mongoDbContainer = new MongoDbBuilder()
-                    .WithImage("mongo:latest")
-                    .Build(); // 默认 MongoDB 端口
-                              // 在容器启动后进行数据库连接初始化
-                              //_mongoDbContainer.StartAsync().Wait();
-            }
-
-            public async Task InitializeAsync()
-            {
-                // 连接到 MongoDB 实例
-                await _mongoDbContainer.StartAsync();
-                ConnectionString = _mongoDbContainer.GetConnectionString();
-            }
-
-            public async Task DisposeAsync()
-            {
-                // 测试完成后关闭容器
-                if (_mongoDbContainer != null)
-                {
-                    await _mongoDbContainer.StopAsync();
-                }
-            }
+            // 创建 MongoDB 容器配置
+            _mongoDbContainer = new MongoDbBuilder()
+                .WithImage("mongo:latest")
+                .Build(); // 默认 MongoDB 端口
         }
 
-        public class IpInformationTests : IClassFixture<MongoDbContainerFixture>
+        public async Task InitializeAsync()
         {
-            private readonly MongoDbContainerFixture _mongoDbContainerFixture;
-            public MongoDbConnector _mongoDbConnector;
-            public MongoIpInformationRepository Repository;
-            public IpInformationTests(MongoDbContainerFixture mongoDbContainerFixture)
+            // 连接到 MongoDB 实例
+            await _mongoDbContainer.StartAsync();
+            ConnectionString = _mongoDbContainer.GetConnectionString();
+        }
+
+        public async Task DisposeAsync()
+        {
+            // 测试完成后关闭容器
+            if (_mongoDbContainer != null)
             {
-                _mongoDbContainerFixture = mongoDbContainerFixture;
-                _mongoDbConnector = new(_mongoDbContainerFixture.ConnectionString, "test");
-                Repository = new(_mongoDbConnector);
+                await _mongoDbContainer.StopAsync();
             }
+        }
+    }
 
-
-            [Fact]
-            public async Task CreateIp_ShouldSuccess()
-            {
-                // 创建 IpInformation 实例
-                var ipInfo = new IpInformation
-                {
-                    IpStart = 123,
-                    IpEnd = 345,
-                    RegionName = "liwan",
-                    CountryName = "china",
-                    CityName = "gz",
-                };
-                await Repository.CreateAsync(ipInfo);
-                var result = await Repository.IpInformation.Find(x => x.IpStart == 123).FirstOrDefaultAsync();
-
-                Assert.NotNull(result);
-                Assert.Equal(ipInfo.IpStart, result.IpStart);
-                Assert.Equal(ipInfo.IpEnd, result.IpEnd);
-                Assert.Equal(ipInfo.CountryName, result.CountryName);
-                Assert.Equal(ipInfo.RegionName, result.RegionName);
-                Assert.Equal(ipInfo.CityName, result.CityName);
-            }
-
-            [Fact]
-            public async Task LookupIp_WhenValidIp_ShouldReturnIpInformation()
-            {
-                IpInformation? IpInfo = await Repository.GetByIpAsync(125);
-                Assert.NotNull(IpInfo);
-                Assert.Equal("gz", IpInfo.CityName);
-            }
-
-            [Fact]
-            public async Task LookupIp_WhenNoRecord_ShouldReturnNull()
-            {
-                IpInformation? IpInfo = await Repository.GetByIpAsync(1000);
-                Assert.Null(IpInfo);
-                //await Assert.ThrowsAsync<InvalidOperationException>(() => Repository.GetByIpAsync(1000));
-            }
+    public class IpInformationTests : IClassFixture<MongoDbContainerFixture>
+    {
+        public MongoIpInformationRepository Repository;
+        public IpInformationTests(MongoDbContainerFixture mongoDbContainerFixture)
+        {
+            MongoDbContainerFixture _mongoDbContainerFixture = mongoDbContainerFixture;
+            MongoDbConnector _mongoDbConnector = new(_mongoDbContainerFixture.ConnectionString, "test");
+            Repository = new(_mongoDbConnector);
         }
 
 
+        [Fact]
+        public async Task CreateIp_ShouldSuccess()
+        {
+            // 创建 IpInformation 实例
+            var ipInfo = new IpInformation
+            {
+                IpStart = 123,
+                IpEnd = 345,
+                RegionName = "liwan",
+                CountryName = "china",
+                CityName = "gz",
+            };
+            await Repository.CreateAsync(ipInfo);
+            var result = await Repository.IpInformation.Find(x => x.IpStart == 123).FirstOrDefaultAsync();
+
+            Assert.NotNull(result);
+            Assert.Equal(ipInfo, result);
+        }
+
+        [Fact]
+        public async Task LookupIp_WhenValidIp_ShouldReturnIpInformation()
+        {
+            var ipInfo = new IpInformation
+            {
+                IpStart = 12345,
+                IpEnd = 34567,
+                RegionName = "liwan1",
+                CountryName = "china",
+                CityName = "gz1",
+            };
+            await Repository.CreateAsync(ipInfo);
+            IpInformation? IpInfo = await Repository.GetByIpAsync(12345);
+            Assert.NotNull(IpInfo);
+            Assert.Equal("gz1", IpInfo.CityName);
+        }
+
+        [Fact]
+        public async Task LookupIp_WhenNoRecord_ShouldReturnNull()
+        {
+            IpInformation? IpInfo = await Repository.GetByIpAsync(1000);
+            Assert.Null(IpInfo);
+            //await Assert.ThrowsAsync<InvalidOperationException>(() => Repository.GetByIpAsync(1000));
+        }
     }
 }
